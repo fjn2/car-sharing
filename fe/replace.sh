cd6c92f2847893e1f0177098dec28c281c63cb33
Federico Naveira <federiconaveira@gmail.com>
2017-12-31 21:19:56 -0300

    Changed replace script

--- fe/replace.sh
+++ fe/replace.sh
@@ -1,3 +1,3 @@
-sed "s/.*const serverUrl.*/const serverUrl = 'http:\/\/naveira.com.ar:3051'/" fe/main.js > fe/main.js2;
-cp fe/main.js2 fe/main.js;
-rm fe/main.js2
+sed "s/.*const serverUrl.*/const serverUrl = 'http:\/\/naveira.com.ar:3051'/" main.js > main.js2;
+cp main.js2 main.js;
+rm main.js2
