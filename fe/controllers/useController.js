7e7f90af4e27a3df13607470ee7d83aa935f19bc
Federico Naveira <federiconaveira@gmail.com>
2018-01-04 13:50:59 -0300

    Added missing changes

--- fe/controllers/useController.js
+++ fe/controllers/useController.js
@@ -10,18 +10,25 @@ const useController = {
   getList: () => (new Promise((resolve, reject) => {
     useSvc.getList().then((useItems) => {
       const list = document.querySelector('#use .list');
-      useItems.forEach((item) => {
+      useItems.forEach((item, index) => {
         const li = document.createElement('li');
         const spanUsername = document.createElement('span');
         const spanAction = document.createElement('span');
         const b = document.createElement('b');
+        const spanDate = document.createElement('span');
         spanUsername.innerText = item.name;
         spanAction.innerText = item.isPick ? 'Pick': 'Leave';
-        b.innerText = item.km + ' km';
+
+        const diffWithPrevious = index ? item.km * 1 - useItems[index - 1].km * 1 : 0;
+
+        b.innerText = item.km + ' km (+' + diffWithPrevious + ')';
+
+        spanDate.innerText = new Date(item.date).toDateString();
 
         li.appendChild(spanUsername);
         li.appendChild(spanAction);
         li.appendChild(b);
+        li.appendChild(spanDate);
 
         list.appendChild(li);
       });
